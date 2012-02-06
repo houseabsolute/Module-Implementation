c0fb3171df0dab3125df46dda23f4676dd4963ba
Dave Rolsky <autarch@urth.org>
2012-02-06 16:21:51 -0600

    Clarify use case

--- lib/Module/Implementation.pm
+++ lib/Module/Implementation.pm
@@ -155,6 +155,10 @@ implementations for a module. This can be used to provide XS and pure Perl
 implementations of a module, or it could be used to load an implementation for
 a given OS or any other case of needing to provide multiple implementations.
 
+This module is only useful when you know all the implementations ahead of
+time. If you want to load arbitrary implementations then you probably want
+something like a plugin system, not this module.
+
 =head1 API
 
 This module provides one subroutine, C<build_loader_sub()>, which is not
