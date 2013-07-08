eb0c4eb53bf97adb65543aadd5305283430daa46
Peter Rabbitson <ribasushi@cpan.org>
2013-07-08 04:49:17 +0200

    Test glob import as well

--- t/lib/T/Impl1.pm
+++ t/lib/T/Impl1.pm
@@ -14,3 +14,8 @@ sub return_package {
 our $SCALAR = 42;
 our @ARRAY  = ( 1, 2, 3 );
 our %HASH   = ( key => 'val' );
+
+open MULTI, '>&STDOUT' or die 'Unable to dup STDOUT';
+our (%MULTI, @MULTI);
+
+1;
--- t/lib/T/Impl2.pm
+++ t/lib/T/Impl2.pm
@@ -15,4 +15,7 @@ our $SCALAR = 42;
 our @ARRAY  = ( 1, 2, 3 );
 our %HASH   = ( key => 'val' );
 
+open MULTI, '>&STDOUT' or die 'Unable to dup STDOUT';
+our (%MULTI, @MULTI);
+
 1;
